577257810adac0ddc29b9f4a1f2aa5d4ff2265e8
jes7923 <jes7923@cs.rit.edu>
2016-04-09 16:22:40 -0400

    Initialization

---
+++ src/LasersPTUI.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Jordan on 4/9/2016.
+ *
+ */
+public class LasersPTUI {
+    public static void main(String[] args) {
+        System.out.println("My name is Jordan Shea");
+        System.out.println("My name is Jennifer Liu");
+        System.out.println("My name is Moses Lagoon");
+        System.out.println("Our project account is p142-03n");
+    }
+}
