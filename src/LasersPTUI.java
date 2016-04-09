7c0f5e7c4b61757ab68ed7ed8be997b14a1b741c
JenniferLiu <jwt7689@rit.edu>
2016-04-09 17:12:38 -0400

    Method Skeleton

--- src/LasersPTUI.java
+++ src/LasersPTUI.java
@@ -13,19 +13,34 @@ import java.io.FileNotFoundException;
  * Jennifer: Add, Errors, Verify
  *
  */
-public class LasersPTUI {
 
-    public static void testMethod(String str){
-        System.out.println(str);
-        System.out.println("YAY");
+public class LasersPTUI {
+    private char[][] grid;
+
+    public LasersPTUI(){
+
     }
+
+    public String display(){
+        return "";
+    }
+
+    public boolean verify(){
+        return false;
+    }
+
+    public char[][] remove(int r, int c){
+        return null;
+    }
+
+    public String add(int r, int c){
+        return "";
+    }
+
     public static void main(String[] args) {
         System.out.println("My name is Jordan Shea");
         System.out.println("My name is Jennifer Liu");
         System.out.println("My name is Moses Lagoon");
         System.out.println("Our project account is p142-03n");
-        testMethod("Please work");
-
-
     }
 }
