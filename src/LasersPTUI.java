fed920cf30234b796fe855919694ec6c3e290285
JenniferLiu <jwt7689@rit.edu>
2016-04-09 16:37:31 -0400

    Initial Check In 3

--- src/LasersPTUI.java
+++ src/LasersPTUI.java
@@ -8,10 +8,7 @@ public class LasersPTUI {
         System.out.println("My name is Jennifer Liu");
         System.out.println("My name is Moses Lagoon");
         System.out.println("Our project account is p142-03n");
- * @author Jennifer Liu
- */
-public class LasersPTUI {
-    public static void main(String[] args){
+
         System.out.println("My name is Jennifer");
         System.out.println("My name is Jordan Shea");
         System.out.println("My name is Moses Lagoon");
