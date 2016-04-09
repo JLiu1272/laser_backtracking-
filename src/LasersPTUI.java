fe54f8d050e6f2f17f42bc34beece1f8b719da11
JenniferLiu <jwt7689@rit.edu>
2016-04-09 16:45:35 -0400

    Main Function Write Up

--- src/LasersPTUI.java
+++ src/LasersPTUI.java
@@ -1,18 +1,15 @@
+import java.io.FileNotFoundException;
+
 /**
  * Created by Jordan on 4/9/2016.
  *
  */
 public class LasersPTUI {
-    public static void main(String[] args) {
-        System.out.println("My name is Jordan Shea");
-        System.out.println("My name is Jennifer Liu");
-        System.out.println("My name is Moses Lagoon");
-        System.out.println("Our project account is p142-03n");
 
-        System.out.println("My name is Jennifer");
-        System.out.println("My name is Jordan Shea");
-        System.out.println("My name is Moses Lagoon");
-        System.out.println("Our project account is p142-03n");
-        System.out.println("Hi");
+
+    public static void main(String[] args) throws FileNotFoundException{
+        if(args.length != 0){
+            System.out.println("Usage: java LasersPTUI safe-file" + args[0]);
+        }
     }
 }
