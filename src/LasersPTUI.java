0f8e99cce134c585cbe9d41341e1daad1acd77a6
Moses <mxl2261@rit.edu>
2016-04-11 17:56:18 -0400

    Pushing initial implementation of Display and Constructor.

--- src/LasersPTUI.java
+++ src/LasersPTUI.java
@@ -1,4 +1,5 @@
 import com.sun.org.apache.xpath.internal.SourceTree;
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -49,18 +50,48 @@ public class LasersPTUI {
             }
         }
 
-        for (int r =0; r <rDIM; r++){
-            for(int c = 0; c < cDIM; c++){
-                String s = in.next();
-                System.out.print(this.grid[r][c]);
-            }
-            System.out.println();
-        }
+
     }
 
-    public String display(){
-        return "";
+    public void display(){
+
+
+        String result = "  ";
+        /** Printing Column Numbers */
+        for (int c = 0; c<cDIM; c++){
+            result += " " + c;        //Prints out Column number here
+        }
+        result+= "\n" + "   ";
+
+        /** TOP ROW DASES "-------" */
+        for(int j = 1; j<cDIM; j++){
+            result += "--";         //TOP ROW DASHES HERE
+        }
+        result += "-\n";            //ONE MORE DASH + NEWLINE
+
+        /** DATA BODY - rows of them */
+        for (int r =0; r <rDIM; r++){
+            result += r + "|";                  // ROW Number and Left side bars
+            for(int c = 0; c < cDIM; c++){
+                result += " " + this.grid[r][c];    //filling the grid in
+            }
+            result += "\n";
+        }
+        System.out.println(result);
+
+
+        //JUST CHECKING WITH SOLUTION:
+        /**
+        System.out.println("  0 1 2 3\n" +
+                "  -------\n" +
+                "0|* L * 0\n" +
+                "1|* X L *\n" +
+                "2|L * 1 *\n" +
+                "3|1 * * L");
+       */
     }
+
+    //MOSES LAGOON
 
     public boolean verify(){
         return false;
@@ -75,21 +106,27 @@ public class LasersPTUI {
     }
 
     public static void main(String[] args) {
-        System.out.println("My name is Jordan Shea");
-        System.out.println("My name is Jennifer Liu");
-        System.out.println("My name is Moses Lagoon");
-        System.out.println("Our project account is p142-03n");
+//        System.out.println("My name is Jordan Shea");
+//        System.out.println("My name is Jennifer Liu");
+//        System.out.println("My name is Moses Lagoon");
+//        System.out.println("Our project account is p142-03n");
         // Jordan Shea
         try {
             if (args.length == 1) {
-                new LasersPTUI(args[0]);
+                //MOSES LAGOON
+                LasersPTUI lasers = new LasersPTUI(args[0]);
+                lasers.display();           //PRINTING DISPLAY HERE
+                //MOSES LAGOON
             } else if (args.length == 2) {
-                new LasersPTUI(args[0]);
-                // Need to create scanner/helper method to read in args[1]
+                //MOSES LAGOON
+                //Creating a new lasers object to print out display
+                LasersPTUI lasers = new LasersPTUI(args[0]);
+                lasers.display();
+                //MOSES LAGOON
             } else {
                 System.out.println("Usage: java LasersPTUI safe-file [input]");
             }
-            
+
         } catch (Exception e){
             System.out.println(e);
         }
